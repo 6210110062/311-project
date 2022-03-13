c1f1fd39f49a7e520a8e1b77a4eb61b76dd8a295
6210110165 <kyokukung@gmail.com>
2022-03-13 22:58:23 +0700

    4th commit(facebook login)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import FacebookLogin from 'react-facebook-login';
+ 
+const responseFacebook = (response) => {
+  console.log(response);
+}
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FacebookLogin
+    appId="524924259001761"
+    autoLoad={true}
+    callback={responseFacebook} />
   );
 }
 
