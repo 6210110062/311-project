04efad4725b6867aa4343179306a3fa767eaaf2c
062 <6210110062@psu.ac.th>
2022-03-13 23:13:55 +0700

    info

--- backend/main.js
+++ backend/main.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const users = require('./info')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
