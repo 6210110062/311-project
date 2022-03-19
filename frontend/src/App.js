3458f6d884d7d9fc497b6eb75f57e6d920970a5c
062 <6210110062@psu.ac.th>
2022-03-19 23:04:15 +0700

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,59 @@
-import FacebookLogin from 'react-facebook-login';
- 
-const responseFacebook = (response) => {
-  console.log(response);
-}
- 
-function App() {
-  return (
-    <FacebookLogin
-    appId="524924259001761"
-    autoLoad={true}
-    callback={responseFacebook} />
-  );
-}
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
 
-export default App;
+export default class Facebook extends Component {
+  state = {
+    isLoggedIn: false,
+    userID: "",
+    name: "",
+    email: "",
+    picture: ""
+  };
+
+  responseFacebook = response => {
+    // console.log(response);
+
+    this.setState({
+      isLoggedIn: true,
+      userID: response.userID,
+      name: response.name,
+      email: response.email,
+      picture: response.picture.data.url
+    });
+  };
+
+  componentClicked = () => console.log("clicked");
+
+  render() {
+    let fbContent;
+
+    if (this.state.isLoggedIn) {
+      fbContent = (
+        <div
+          style={{
+            width: "400px",
+            margin: "auto",
+            background: "#f4f4f4",
+            padding: "20px"
+          }}
+        >
+          <img src={this.state.picture} alt={this.state.name} />
+          <h2>Welcome {this.state.name}</h2>
+          Email: {this.state.email}
+        </div>
+      );
+    } else {
+      fbContent = (
+        <FacebookLogin
+          appId="1111795056055256"
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={this.componentClicked}
+          callback={this.responseFacebook}
+        />
+      );
+    }
+
+    return <div>{fbContent}</div>;
+  }
+}
